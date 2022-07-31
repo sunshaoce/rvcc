6720c912790b43e5c6adb0b73db95cef478b574d
Shao-Ce SUN <sunshaoce@iscas.ac.cn>
2022-07-31 20:58:35 +0800

    [2] 支持+ -运算符

--- main.c
+++ main.c
@@ -13,15 +13,51 @@ int main(int Argc, char **Argv) {
     return 1;
   }
 
+  // P保存着输入的算式的str
+  char *P = Argv[1];
+
   // 声明一个全局main段，同时也是程序入口段
   printf("  .globl main\n");
   // main段标签
   printf("main:\n");
   // li为addi别名指令，加载一个立即数到寄存器中
-  // 传入程序的参数为str类型，因为需要转换为需要int类型，
-  // atoi为“ASCII to integer”
-  printf("  li a0, %d\n", atoi(Argv[1]));
+
+  // 传入程序的参数为str类型，因为需要转换为需要long类型
+  // strtol为“string to long”，
+  // 参数为：被转换的str，str除去数字后的剩余部分，进制
+  // 传入&P，即char**, 是为了修改P的值
+
+  // 这里我们将算式分解为 num (op num) (op num)...的形式
+  // 所以先将第一个num传入a0
+  printf("  li a0, %ld\n", strtol(P, &P, 10));
+
+  // 解析 (op num)
+  // *P在这里判断P是否为NULL
+  while (*P) {
+    // 解析op
+    if (*P == '+') {
+      ++P; // 跳过‘+’
+      // 解析num
+      // addi rd, rs1, imm 表示 rd = rs1 + imm
+      printf("  addi a0, a0, %ld\n", strtol(P, &P, 10));
+      continue;
+    }
+
+    if (*P == '-') {
+      ++P;
+      // 解析num
+      // addi中imm为有符号立即数，所以减法表示为 rd = rs1 + (-imm)
+      printf("  addi a0, a0, -%ld\n", strtol(P, &P, 10));
+      continue;
+    }
+
+    // 如果存在未解析的字符，则报错
+    fprintf(stderr, "unexpected character: '%c'\n", *P);
+    return 1;
+  }
+
   // ret为jalr x0, x1, 0别名指令，用于返回子程序
+  // 返回的为a0的值
   printf("  ret\n");
 
   return 0;
--- test.sh
+++ test.sh
@@ -36,5 +36,8 @@ assert() {
 assert 0 0
 assert 42 42
 
+# [2] 支持+ -运算符
+assert 34 '12-34+56'
+
 # 如果运行正常未提前退出，程序将显示OK
 echo OK
