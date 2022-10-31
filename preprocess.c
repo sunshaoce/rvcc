c2e28d98b0031d7b07a421be424af7ce0582ac2c
Shao-Ce SUN <sunshaoce@iscas.ac.cn>
2022-10-31 22:59:49 +0800

    [252] 忽略#pragma

--- preprocess.c
+++ preprocess.c
@@ -1026,6 +1026,14 @@ static Token *preprocess2(Token *Tok) {
       continue;
     }
 
+    // 匹配#pragma
+    if (equal(Tok, "pragma")) {
+      do {
+        Tok = Tok->Next;
+      } while (!Tok->AtBOL);
+      continue;
+    }
+
     // 匹配#error
     if (equal(Tok, "error"))
       errorTok(Tok, "error");
