a6fc2f23888c0fc5d5808adc9d2d37556b7cfc16
Shao-Ce SUN <sunshaoce@iscas.ac.cn>
2022-10-31 19:07:42 +0800

    [245] 改进多字节字符的报错信息

--- rvcc.h
+++ rvcc.h
@@ -402,6 +402,7 @@ bool isIdent2_1(uint32_t C);
 int encodeUTF8(char *Buf, uint32_t C);
 // 将UTF-8的格式解码为unicode字符
 uint32_t decodeUTF8(char **NewPos, char *P);
+int displayWidth(char *P, int Len);
 
 //
 // 主程序，驱动文件
