3d3093b931b4c3b7c465c0582b243e2fd72aa9a7
Shao-Ce SUN <sunshaoce@iscas.ac.cn>
2022-10-31 22:21:41 +0800

    [246] 支持#line

---
+++ test/line.c
@@ -0,0 +1,19 @@
+#include "test.h"
+
+int main() {
+
+  printf("[246] 支持#line\n");
+#line 500 "foo"
+  ASSERT(501, __LINE__);
+  ASSERT(0, strcmp(__FILE__, "foo"));
+
+#line 800 "bar"
+  ASSERT(801, __LINE__);
+  ASSERT(0, strcmp(__FILE__, "bar"));
+
+#line 1
+  ASSERT(2, __LINE__);
+
+  printf("OK\n");
+  return 0;
+}
