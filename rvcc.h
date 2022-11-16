0606ea60214880d9ed79a0cdf723fe0b0283725d
Shao-Ce SUN <sunshaoce@iscas.ac.cn>
2022-11-16 10:23:53 +0800

    [189] 支持 __FILE__ 和 __LINE__

--- rvcc.h
+++ rvcc.h
@@ -86,6 +86,7 @@ struct Token {
   bool AtBOL;       // 终结符在行首（begin of line）时为true
   bool HasSpace;    // 终结符前是否有空格
   Hideset *Hideset; // 用于宏展开时的隐藏集
+  Token *Origin;    // 宏展开前的原始终结符
 };
 
 // 去除了static用以在多个文件间访问
--- test/include1.h
+++ test/include1.h
@@ -1,4 +1,8 @@
 #include "include2.h"
 
+// [189] 支持 __FILE__ 和 __LINE__
+char *include1_filename = __FILE__;
+int include1_line = __LINE__;
+
 // [160] 支持 #include "..."
 int include1 = 5;
