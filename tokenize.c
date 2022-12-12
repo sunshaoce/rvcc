ec08e14df1254bd92d3ed885e91f6ec32895515d
Shao-Ce SUN <sunshaoce@iscas.ac.cn>
2022-12-12 13:49:08 +0800

    [226] 支持UTF-16字符字面量

--- tokenize.c
+++ tokenize.c
@@ -305,7 +305,7 @@ static Token *readStringLiteral(char *Start) {
 }
 
 // 读取字符字面量
-static Token *readCharLiteral(char *Start, char *Quote) {
+static Token *readCharLiteral(char *Start, char *Quote, Type *Ty) {
   char *P = Quote + 1;
   // 解析字符为 \0 的情况
   if (*P == '\0')
@@ -328,7 +328,7 @@ static Token *readCharLiteral(char *Start, char *Quote) {
   // 构造一个NUM的终结符，值为C的数值
   Token *Tok = newToken(TK_NUM, Start, End + 1);
   Tok->Val = C;
-  Tok->Ty = TyInt;
+  Tok->Ty = Ty;
   return Tok;
 }
 
@@ -563,7 +563,7 @@ Token *tokenize(File *FP) {
 
     // 解析字符字面量
     if (*P == '\'') {
-      Cur->Next = readCharLiteral(P, P);
+      Cur->Next = readCharLiteral(P, P, TyInt);
       // 单字节字符
       Cur->Val = (char)Cur->Val;
       Cur = Cur->Next;
@@ -571,9 +571,18 @@ Token *tokenize(File *FP) {
       continue;
     }
 
+    // UTF-16字符字面量
+    if (startsWith(P, "u'")) {
+      // 使用两个字节
+      Cur = Cur->Next = readCharLiteral(P, P + 1, TyUShort);
+      Cur->Val &= 0xffff;
+      P += Cur->Len;
+      continue;
+    }
+
     // 宽字符字面量，占两个字节
     if (startsWith(P, "L'")) {
-      Cur = Cur->Next = readCharLiteral(P, P + 1);
+      Cur = Cur->Next = readCharLiteral(P, P + 1, TyInt);
       P += Cur->Len;
       continue;
     }
