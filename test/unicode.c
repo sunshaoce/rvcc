85237721d0725dbf9d0d2b1c6b8e073d9d2411aa
Shao-Ce SUN <sunshaoce@iscas.ac.cn>
2022-12-12 13:50:31 +0800

    [227] 支持UTF-32字符字面量

--- test/unicode.c
+++ test/unicode.c
@@ -31,6 +31,16 @@ int main() {
 
   ASSERT(0, strcmp(STR(u'a'), "u'a'"));
 
+  printf("[227] 支持UTF-32字符字面量\n");
+  ASSERT(4, sizeof(U'\0'));
+  ASSERT(1, U'\xffffffff' >> 31);
+  ASSERT(97, U'a');
+  ASSERT(946, U'β');
+  ASSERT(12354, U'あ');
+  ASSERT(127843, U'🍣');
+
+  ASSERT(0, strcmp(STR(U'a'), "U'a'"));
+
   printf("OK\n");
   return 0;
 }
